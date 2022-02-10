02d381084b6bc81f49976c6a2aaee307408ca1f9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-10 12:29:48 -0500

    :memo: Link my blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ const Home: NextPage = () => {
   return (
     <div>
       <p>Work in progress</p>
+      <p>Go to my blog while in work on this</p>
+      <p>https://blog.ultirequiem.com/</p>
     </div>
   );
 };
